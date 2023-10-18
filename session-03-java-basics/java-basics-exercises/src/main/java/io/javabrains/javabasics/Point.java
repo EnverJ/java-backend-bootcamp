e67f293e75041bdc9828216697cd0a82f62e9773
EnverJ <Enlem1987@gmail.com>
2023-10-17 21:46:40 -0600

    point

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Point.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Point.java
@@ -1,2 +1,12 @@
-package io.javabrains.javabasics;public class Point {
+package io.javabrains.javabasics;
+
+public record Point(int x, int y) {
+    // instead of creating getter and setter, just use record
+    // fields are automatically generated as private final
+    // constructor is automatically generated to initialize all fields
+    // automatically gets methods like 'equals()', 'hasCode()', and 'toString()'
+    // derived from _all_of the component fields.
+    // not mean to replace to classes
+    //
+
 }
